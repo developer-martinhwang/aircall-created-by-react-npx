21343e37e583190f3b65c63593c404c3f519df86
Martin Hwang <mhwang@omics.com>
2022-07-22 09:14:17 -0300

    updated all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+// mui v4
+import { Box } from '@material-ui/core';
+// components
+import Header from './components/moudle/Header.js';
+import AllAirCalls from './components/page/AllAirCalls.js';
+import InBoxAirCalls from './components/page/InBoxAirCalls.js';
+import ArchiveAirCalls from './components/page/ArchiveAirCalls.js';
+import Footer from './components/moudle/Footer.js';
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className='container'>
+      <BrowserRouter>
+          <Header/>
+          <Box className='body'>
+            <Routes>
+              <Route index element={<AllAirCalls />} />
+              <Route path="/inbox-aircalls" element={<InBoxAirCalls/>}/>
+              <Route path="/all-aircalls" element={<AllAirCalls/>}/>
+              <Route path="/archive-aircalls" element={<ArchiveAirCalls/>}/>
+            </Routes>
+          </Box>
+          <Footer/>
+      </BrowserRouter>
+    </Box>
   );
-}
-
+};
 export default App;
